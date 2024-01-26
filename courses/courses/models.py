e34a8feb8699744687fe857484b416f3f04ae958
pranavrai070 <pranavrai070@gmail.com>
2024-01-26 16:33:57 +0530

    course route added and migrated

--- courses/courses/models.py
+++ courses/courses/models.py
@@ -4,12 +4,21 @@ class Course(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 class Assessment(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     questions = models.TextField()
+
+    def __str__(self):
+        return self.title
--- courses/courses/views.py
+++ courses/courses/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Course, Lesson, Assessment
 from .serializers import CourseSerializer, LessonSerializer, AssessmentSerializer
 
@@ -13,4 +16,8 @@ class LessonViewSet(viewsets.ModelViewSet):
 class AssessmentViewSet(viewsets.ModelViewSet):
     queryset = Assessment.objects.all()
     serializer_class = AssessmentSerializer
-    
+
+@api_view(['GET','POST'])
+def drink_list(request):
+    print(request.method)
+    return JsonResponse({"Successfully hitting the Courses Route":"True"})
